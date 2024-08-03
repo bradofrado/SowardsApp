dcb7f9a72bd236bb7a41b00452147a8334752bbe
Braydon Jones <92066133+bradofrado@users.noreply.github.com>
2024-08-03 06:45:51 -0600

    Update actions.ts

--- apps/vacations/app/plan/actions.ts
+++ apps/vacations/app/plan/actions.ts
@@ -35,7 +35,7 @@ export const generateItinerary = async (date: Date): Promise<string> => {
 };
 
 const getLocationFromDate = (date: Date) => {
-  return isDateInBetween(date, new Date(), new Date("2024-08-07"))
+  return isDateInBetween(date, new Date("2024-08-01"), new Date("2024-08-07"))
     ? {lat: 21.64, long: -157.91 }
     : {lat: 19.93, long: -155.78}
 };
