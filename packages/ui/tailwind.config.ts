06f9e2ca38bf20aecc8a82e0a454c0c0b2fba1f5
Braydon Jones <bradofrado@gmail.com>
2024-08-17 21:57:53 -0600

    Fix build errors

--- packages/ui/tailwind.config.ts
+++ packages/ui/tailwind.config.ts
@@ -4,10 +4,17 @@ import type { ForceValueType } from "model/src/core/utils";
 import baseConfig from "tailwind-config/tailwind.config";
 import type { Config } from "tailwindcss";
 import resolveConfig from "tailwindcss/resolveConfig";
-import tailwindColors from 'tailwindcss/colors'
+import tailwindColors from "tailwindcss/colors";
 import type { DefaultColors } from "tailwindcss/types/generated/colors";
 
 export default baseConfig satisfies Config;
 
 const fullConfig = resolveConfig(baseConfig);
-export const colors = {...tailwindColors, ...baseConfig.theme.extend.colors} as unknown as ForceValueType<typeof fullConfig.theme.extend.colors & Omit<DefaultColors, 'gray'>, string, HexColor>;
+export const colors = {
+  ...tailwindColors,
+  ...baseConfig.theme.extend.colors,
+} as unknown as ForceValueType<
+  typeof fullConfig.theme.colors & Omit<DefaultColors, "gray">,
+  string,
+  HexColor
+>;
